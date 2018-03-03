13964217f4fbb3239c361e582c5ad6076c1f9328
Mikołaj Król <mikolaj.krol@bitbag.pl>
2018-03-04 00:46:21 +0100

    Prepare filter view

--- src/Controller/Action/Shop/ListProductsAction.php
+++ src/Controller/Action/Shop/ListProductsAction.php
@@ -14,12 +14,19 @@ namespace BitBag\SyliusElasticsearchPlugin\Controller\Action\Shop;
 
 use BitBag\SyliusElasticsearchPlugin\Controller\RequestDataHandler\DataHandlerInterface;
 use BitBag\SyliusElasticsearchPlugin\Finder\FinderInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use BitBag\SyliusElasticsearchPlugin\Form\Type\ShopProductsFilterType;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 final class ListProductsAction
 {
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+
     /**
      * @var DataHandlerInterface
      */
@@ -31,16 +38,27 @@ final class ListProductsAction
     private $shopProductsFinder;
 
     /**
+     * @var EngineInterface
+     */
+    private $templatingEngine;
+
+    /**
+     * @param FormFactoryInterface $formFactory
      * @param DataHandlerInterface $shopProductListDataHandler
      * @param FinderInterface $shopProductsFinder
+     * @param EngineInterface $templatingEngine
      */
     public function __construct(
+        FormFactoryInterface $formFactory,
         DataHandlerInterface $shopProductListDataHandler,
-        FinderInterface $shopProductsFinder
+        FinderInterface $shopProductsFinder,
+        EngineInterface $templatingEngine
     )
     {
+        $this->formFactory = $formFactory;
         $this->shopProductListDataHandler = $shopProductListDataHandler;
         $this->shopProductsFinder = $shopProductsFinder;
+        $this->templatingEngine = $templatingEngine;
     }
 
     /**
@@ -50,9 +68,16 @@ final class ListProductsAction
      */
     public function __invoke(Request $request): Response
     {
+        $form = $this->formFactory->create(ShopProductsFilterType::class);
         $data = $this->shopProductListDataHandler->retrieveData($request);
-        $result = $this->shopProductsFinder->find($data);
+        $products = $this->shopProductsFinder->find($data);
+        $template = $request->get('template');
 
-        return new JsonResponse($result);
+        $form->handleRequest($request);
+
+        return $this->templatingEngine->renderResponse($template, [
+            'form' => $form->createView(),
+            'products' => $products,
+        ]);
     }
 }
