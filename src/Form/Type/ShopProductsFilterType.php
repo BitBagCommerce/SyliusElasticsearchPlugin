7a9467b3830ca5d4dd365f2784521a5d1587b35f
Mikołaj Król <mikolaj.krol@bitbag.pl>
2018-03-06 14:20:53 +0100

    Remove embedeed form

--- src/Form/Type/ShopProductsFilterType.php
+++ src/Form/Type/ShopProductsFilterType.php
@@ -12,17 +12,31 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusElasticsearchPlugin\Form\Type;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 final class ShopProductsFilterType extends AbstractFilterType
 {
+    /**
+     * @var string
+     */
+    private $nameProperty;
+
+    /**
+     * @param string $nameProperty
+     */
+    public function __construct(string $nameProperty)
+    {
+        $this->nameProperty = $nameProperty;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', NameFilterType::class)
+            ->add($this->nameProperty, TextType::class)
         ;
     }
 }
