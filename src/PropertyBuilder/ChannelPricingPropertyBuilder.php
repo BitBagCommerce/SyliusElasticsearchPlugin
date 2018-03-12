59f96f2f3d85d770b38915223c79d7cbbdc3f43b
Mikołaj Król <mikolaj.krol@bitbag.pl>
2018-03-12 12:23:37 +0100

    Add channel pricing property

---
+++ src/PropertyBuilder/ChannelPricingPropertyBuilder.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file has been created by developers from BitBag. 
+ * Feel free to contact us once you face any issues or want to start
+ * another great project. 
+ * You can find more information about us on https://bitbag.shop and write us
+ * an email on mikolaj.krol@bitbag.pl. 
+ */
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusElasticsearchPlugin\PropertyBuilder;
+
+use FOS\ElasticaBundle\Event\TransformEvent;
+use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Component\Core\Model\ProductVariantInterface;
+
+final class ChannelPricingPropertyBuilder extends AbstractPropertyBuilder
+{
+    /**
+     * @var string
+     */
+    private $priceProperty;
+
+    /**
+     * @param string $priceProperty
+     */
+    public function __construct(string $priceProperty)
+    {
+        $this->priceProperty = $priceProperty;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildProperty(TransformEvent $event): void
+    {
+        $product = $event->getObject();
+
+        if (!$product instanceof ProductInterface) {
+            return;
+        }
+
+        $document = $event->getDocument();
+
+        /** @var ProductVariantInterface $productVariant */
+        foreach ($product->getVariants() as $productVariant) {
+            foreach($productVariant->getChannelPricings() as $channelPricing) {
+                $propertyName = $this->priceProperty . '_' . strtolower($channelPricing->getChannelCode());
+
+                $document->set($propertyName, $channelPricing->getPrice());
+            }
+        }
+    }
+}
