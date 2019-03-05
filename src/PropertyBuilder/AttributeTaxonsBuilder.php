79a31bd2c42df3cd91e958480ccc96aa4eeab9ee
psihius <arvids.godjuks@gmail.com>
2019-03-05 14:00:23 +0200

    Fixed OOM that happends in AtributeTaxonBuilder

--- src/PropertyBuilder/AttributeTaxonsBuilder.php
+++ src/PropertyBuilder/AttributeTaxonsBuilder.php
@@ -8,33 +8,20 @@
  * an email on mikolaj.krol@bitbag.pl.
  */
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace BitBag\SyliusElasticsearchPlugin\PropertyBuilder;
 
-use BitBag\SyliusElasticsearchPlugin\PropertyBuilder\Mapper\ProductTaxonsMapperInterface;
+use BitBag\SyliusElasticsearchPlugin\EntityRepository\TaxonRepository as BitbagTaxonRepository;
 use FOS\ElasticaBundle\Event\TransformEvent;
 use Sylius\Component\Attribute\Model\AttributeInterface;
-use Sylius\Component\Core\Model\ProductInterface;
-use Sylius\Component\Product\Model\ProductAttributeValueInterface;
-use Sylius\Component\Product\Repository\ProductAttributeValueRepositoryInterface;
 
 final class AttributeTaxonsBuilder extends AbstractBuilder
 {
     /**
-     * @var ProductAttributeValueRepositoryInterface
+     * @var BitbagTaxonRepository
      */
-    private $productAttributeValueRepository;
-
-    /**
-     * @var ProductTaxonsMapperInterface
-     */
-    private $productTaxonsMapper;
-
-    /**
-     * @var string
-     */
-    private $attributeProperty;
+    protected $bitbagTaxonRepository;
 
     /**
      * @var string
@@ -47,24 +34,18 @@ final class AttributeTaxonsBuilder extends AbstractBuilder
     private $excludedAttributes;
 
     /**
-     * @param ProductAttributeValueRepositoryInterface $productAttributeValueRepository
-     * @param ProductTaxonsMapperInterface $productTaxonsMapper
-     * @param string $attributeProperty
-     * @param string $taxonsProperty
-     * @param array $excludedAttributes
+     * @param BitbagTaxonRepository $bitbagTaxonRepository
+     * @param string                $taxonsProperty
+     * @param array                 $excludedAttributes
      */
     public function __construct(
-        ProductAttributeValueRepositoryInterface $productAttributeValueRepository,
-        ProductTaxonsMapperInterface $productTaxonsMapper,
-        string $attributeProperty,
+        BitbagTaxonRepository $bitbagTaxonRepository,
         string $taxonsProperty,
         array $excludedAttributes = []
     ) {
-        $this->productAttributeValueRepository = $productAttributeValueRepository;
-        $this->productTaxonsMapper = $productTaxonsMapper;
-        $this->attributeProperty = $attributeProperty;
-        $this->taxonsProperty = $taxonsProperty;
-        $this->excludedAttributes = $excludedAttributes;
+        $this->bitbagTaxonRepository = $bitbagTaxonRepository;
+        $this->taxonsProperty        = $taxonsProperty;
+        $this->excludedAttributes    = $excludedAttributes;
     }
 
     /**
@@ -80,20 +61,15 @@ final class AttributeTaxonsBuilder extends AbstractBuilder
             return;
         }
 
-        $document = $event->getDocument();
-        $productAttributes = $this->productAttributeValueRepository->findBy(['attribute' => $documentAttribute]);
-        $taxons = [];
+        $taxons = $this->bitbagTaxonRepository->getTaxonsByAttributeViaProduct($documentAttribute);
 
-        /** @var ProductAttributeValueInterface $attributeValue */
-        foreach ($productAttributes as $attributeValue) {
-            /** @var ProductInterface $product */
-            $product = $attributeValue->getProduct();
-
-            if ($documentAttribute === $attributeValue->getAttribute() && $product->isEnabled()) {
-                $taxons = $this->productTaxonsMapper->mapToUniqueCodes($product);
-            }
+        $taxonCodes = [];
+        foreach ($taxons as $taxon) {
+            $taxonCodes[] = $taxon->getCode();
         }
 
-        $document->set($this->taxonsProperty, $taxons);
+        $document = $event->getDocument();
+
+        $document->set($this->taxonsProperty, $taxonCodes);
     }
 }
