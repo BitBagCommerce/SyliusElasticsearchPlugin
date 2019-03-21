11e68915f9e27688b12cbd2866e538eab28fd5a9
Mikołaj Król <mikolaj.krol@bitbag.pl>
2019-03-21 12:19:29 +0100

    Transform services in src from YAML to XML

---
+++ src/DependencyInjection/BitBagSyliusElasticsearchExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusElasticsearchPlugin\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+final class BitBagSyliusElasticsearchExtension extends Extension
+{
+    public function load(array $config, ContainerBuilder $container): void
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+
+        $loader->load('services.xml');
+    }
+}
