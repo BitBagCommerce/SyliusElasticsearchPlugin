0d14e50b2e0a4cafb0963a6542ab938961ad3168
Arvids Godjuks <psihius@users.noreply.github.com>
2019-03-28 15:00:06 +0200

    Filter form OOM fix (#1)
    OOM fix for attribute form building

--- src/Form/Type/ProductAttributesFilterType.php
+++ src/Form/Type/ProductAttributesFilterType.php
@@ -29,19 +29,28 @@ final class ProductAttributesFilterType extends AbstractFilterType
     /** @var ProductAttributesMapperInterface */
     private $productAttributesMapper;
 
+    /** @var array */
+    protected $excludedAttributes;
+
     public function __construct(
         ProductAttributesContextInterface $productAttributesContext,
         ConcatedNameResolverInterface $attributeNameResolver,
-        ProductAttributesMapperInterface $productAttributesMapper
+        ProductAttributesMapperInterface $productAttributesMapper,
+        array $excludedAttributes
     ) {
         $this->productAttributesContext = $productAttributesContext;
         $this->attributeNameResolver = $attributeNameResolver;
         $this->productAttributesMapper = $productAttributesMapper;
+        $this->excludedAttributes = $excludedAttributes;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $attributes): void
     {
         foreach ($this->productAttributesContext->getAttributes() as $productAttribute) {
+            if (in_array($productAttribute->getCode(), $this->excludedAttributes)) {
+                continue;
+            }
+
             $name = $this->attributeNameResolver->resolvePropertyName($productAttribute->getCode());
             $choices = $this->productAttributesMapper->mapToChoices($productAttribute);
 
