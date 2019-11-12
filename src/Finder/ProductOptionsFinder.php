47f07c85c2008a5d89db604ac204d250212e6e9b
Nicholas Kroll <mikolaj.krol@bitbag.pl>
2019-11-12 23:01:42 +0100

    Hotfix - fix default finder limit

--- src/Finder/ProductOptionsFinder.php
+++ src/Finder/ProductOptionsFinder.php
@@ -44,6 +44,6 @@ final class ProductOptionsFinder implements ProductOptionsFinderInterface
 
         $query = $this->productOptionsByTaxonQueryBuilder->buildQuery($data);
 
-        return $this->optionsFinder->find($query);
+        return $this->optionsFinder->find($query, PHP_INT_MAX);
     }
 }
