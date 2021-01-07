49c897563c7a3b9703c46a6a96ee2a467e7c1586
Patryk Grobelny <crackgm@gmail.com>
2021-01-07 11:05:50 +0100

    Updated to Sylius 1.8

---
+++ .github/elasticsearch/run-elasticsearch.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+if [[ -z $STACK_VERSION ]]; then
+  echo -e "\033[31;1mERROR:\033[0m Required environment variable [STACK_VERSION] not set\033[0m"
+  exit 1
+fi
+
+MAJOR_VERSION=`echo ${STACK_VERSION} | cut -c 1`
+
+docker network create elastic
+
+for (( node=1; node<=${NODES-1}; node++ ))
+do
+  port_com=$((9300 + $node - 1))
+  UNICAST_HOSTS+="es$node:${port_com},"
+done
+
+for (( node=1; node<=${NODES-1}; node++ ))
+do
+  port=$((PORT + $node - 1))
+  port_com=$((9300 + $node - 1))
+  if [ "x${MAJOR_VERSION}" == 'x5' ]; then
+    docker run \
+      --rm \
+      --env "node.name=es${node}" \
+      --env "cluster.name=docker-elasticsearch" \
+      --env "cluster.routing.allocation.disk.threshold_enabled=false" \
+      --env "bootstrap.memory_lock=true" \
+      --env "ES_JAVA_OPTS=-Xms1g -Xmx1g" \
+      --env "xpack.security.enabled=false" \
+      --env "http.port=${port}" \
+      --ulimit nofile=262144:262144 \
+      --ulimit memlock=-1:-1 \
+      --publish "${port}:${port}" \
+      --detach \
+      --network elastic \
+      --name="es${node}" \
+      docker.elastic.co/elasticsearch/elasticsearch:${STACK_VERSION}
+  fi
+done
+
+docker run \
+  --network elastic \
+  --rm \
+  appropriate/curl \
+  --max-time 120 \
+  --retry 120 \
+  --retry-delay 1 \
+  --retry-connrefused \
+  --show-error \
+  --silent \
+  http://es1:$PORT
+
+sleep 10
+
+echo "Elasticsearch up and running"
