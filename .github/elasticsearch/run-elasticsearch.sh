b55b8672f9f2dd3152517eef0c0553ac6d92c3ab
Patryk Grobelny <crackgm@gmail.com>
2021-01-07 11:26:53 +0100

    Github actions Elasticsearch 6.x

--- .github/elasticsearch/run-elasticsearch.sh
+++ .github/elasticsearch/run-elasticsearch.sh
@@ -21,7 +21,7 @@ for (( node=1; node<=${NODES-1}; node++ ))
 do
   port=$((PORT + $node - 1))
   port_com=$((9300 + $node - 1))
-  if [ "x${MAJOR_VERSION}" == 'x5' ]; then
+  if [ "x${MAJOR_VERSION}" == 'x6' ]; then
     docker run \
       --rm \
       --env "node.name=es${node}" \
@@ -30,12 +30,36 @@ do
       --env "bootstrap.memory_lock=true" \
       --env "ES_JAVA_OPTS=-Xms1g -Xmx1g" \
       --env "xpack.security.enabled=false" \
+      --env "xpack.license.self_generated.type=basic" \
+      --env "discovery.zen.ping.unicast.hosts=${UNICAST_HOSTS}" \
+      --env "discovery.zen.minimum_master_nodes=${NODES}" \
       --env "http.port=${port}" \
-      --ulimit nofile=262144:262144 \
+      --ulimit nofile=65536:65536 \
+      --ulimit memlock=-1:-1 \
+      --publish "${port}:${port}" \
+      --publish "${port_com}:${port_com}" \
+      --detach \
+      --network=elastic \
+      --name="es${node}" \
+      docker.elastic.co/elasticsearch/elasticsearch:${STACK_VERSION}
+  elif [ "x${MAJOR_VERSION}" == 'x7' ] || [ "x${MAJOR_VERSION}" == 'x8' ]; then
+    docker run \
+      --rm \
+      --env "node.name=es${node}" \
+      --env "cluster.name=docker-elasticsearch" \
+      --env "cluster.initial_master_nodes=es1" \
+      --env "discovery.seed_hosts=es1" \
+      --env "cluster.routing.allocation.disk.threshold_enabled=false" \
+      --env "bootstrap.memory_lock=true" \
+      --env "ES_JAVA_OPTS=-Xms1g -Xmx1g" \
+      --env "xpack.security.enabled=false" \
+      --env "xpack.license.self_generated.type=basic" \
+      --env "http.port=${port}" \
+      --ulimit nofile=65536:65536 \
       --ulimit memlock=-1:-1 \
       --publish "${port}:${port}" \
       --detach \
-      --network elastic \
+      --network=elastic \
       --name="es${node}" \
       docker.elastic.co/elasticsearch/elasticsearch:${STACK_VERSION}
   fi
