aaddf1cf9d2178188784a8bd0eeae3770f382f96
Bartosz Wojdałowicz <bartoszwojdalowicz@gmail.com>
2021-04-23 14:58:01 +0200

    fix

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -57,5 +57,4 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Sylius\Bundle\ApiBundle\SyliusApiBundle::class => ['all' => true],
     SyliusLabs\DoctrineMigrationsExtraBundle\SyliusLabsDoctrineMigrationsExtraBundle::class => ['all' => true],
-    Symplify\ConsoleColorDiff\ConsoleColorDiffBundle::class => ['dev' => true, 'test' => true],
 ];
