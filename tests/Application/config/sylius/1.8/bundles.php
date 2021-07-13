99ed524d30fde1022674f6c843488de7f9305714
christopherhero <christopherheroo@gmail.com>
2021-07-13 16:40:51 +0200

    [fix github actions]

--- tests/Application/config/sylius/1.8/bundles.php
+++ tests/Application/config/sylius/1.8/bundles.php
@@ -3,4 +3,6 @@
 return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true],
+    FOS\OAuthServerBundle\FOSOAuthServerBundle::class => ['all' => true],
+    Sylius\Bundle\AdminApiBundle\SyliusAdminApiBundle::class => ['all' => true],
 ];
--- tests/Application/config/sylius/1.9/bundles.php
+++ tests/Application/config/sylius/1.9/bundles.php
@@ -3,4 +3,6 @@
 return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
+    FOS\OAuthServerBundle\FOSOAuthServerBundle::class => ['all' => true],
+    Sylius\Bundle\AdminApiBundle\SyliusAdminApiBundle::class => ['all' => true],
 ];
