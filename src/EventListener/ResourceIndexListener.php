96ed26f50c810d50da26ab73773a6e4a156cd132
Jakub Macioszek <jakub.macioszek@bitbag.pl>
2021-08-17 14:49:01 +0200

    Remove bad headers

--- src/EventListener/ResourceIndexListener.php
+++ src/EventListener/ResourceIndexListener.php
@@ -7,11 +7,7 @@
 */
 
 declare(strict_types=1);
-/*
- * This file was created by developers working at BitBag
- * Do you need more information about us and what we do? Visit our https://bitbag.io website!
- * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
-*/
+
 
 namespace BitBag\SyliusElasticsearchPlugin\EventListener;
 
