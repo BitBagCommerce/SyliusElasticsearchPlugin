e084ec4e07f90c42434b3ad36ce30c828e09f6c1
kuba-end <kostek660i@gmail.com>
2021-12-01 10:01:33 +0100

    Add compiler pass, fix elastica match function

---
+++ src/CompillerPass/AuthenticationManagerPolyfillPass.php
@@ -0,0 +1,30 @@
+<?php
+/*
+
+This file was created by developers working at BitBag
+
+Do you need more information about us and what we do? Visit our   website!
+
+We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+*/
+declare(strict_types=1);
+
+namespace BitBag\SyliusElasticsearchPlugin\CompillerPass;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+final class AuthenticationManagerPolyfillPass implements CompilerPassInterface
+{
+
+    public function process(ContainerBuilder $container)
+    {
+        if (
+            $container->has('security.authentication_manager') === false
+            &&
+            $container->has('security.authentication.manager') === true
+        ) {
+            $container->setAlias('security.authentication_manager', 'security.authentication.manager');
+        }
+    }
+}
