b7feea253e079a5a08733e41cb3409635866a591
zalmanix <zalmanix@gmail.com>
2021-12-09 13:34:32 +0100

    not fixed

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -57,4 +57,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Sylius\Bundle\ApiBundle\SyliusApiBundle::class => ['all' => true],
     SyliusLabs\DoctrineMigrationsExtraBundle\SyliusLabsDoctrineMigrationsExtraBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
