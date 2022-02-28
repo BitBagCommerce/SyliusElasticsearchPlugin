199fb8d3459c5ba49d64043bc1be0485ff8af45f
Michał Szczepaniak <m.szczepaniak.000@gmail.com>
2022-02-28 10:27:12 +0100

    Fix sylius 1.11 test application

--- tests/Application/config/sylius/1.11/bundles.php
+++ tests/Application/config/sylius/1.11/bundles.php
@@ -5,4 +5,5 @@ declare(strict_types=1);
 return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
+    Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
 ];
