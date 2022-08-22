342a913c26c3673b91c0a825cd8945bd08e41a9d
patryk <patryk.pogorzelski@bitbag.pl>
2022-08-22 14:57:43 +0200

    add php unit test

---
+++ tests/PHPUnit/Integration/IntegrationTestCase.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file was created by developers working at BitBag
+ * Do you need more information about us and what we do? Visit our https://bitbag.io website!
+ * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+*/
+
+declare(strict_types=1);
+namespace Tests\BitBag\SyliusElasticsearchPlugin\Integration;
+
+
+use ApiTestCase\JsonApiTestCase;
+
+abstract class IntegrationTestCase extends JsonApiTestCase
+{
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+
+        $this->dataFixturesPath = __DIR__ . '/DataFixtures/ORM';
+    }
+
+    protected function setUp(): void
+    {
+
+    }
+
+    protected function tearDown(): void
+    {
+
+    }
+}
---
+++ tests/PHPUnit/Integration/Repository/ProductAttributeValueRepositoryTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\BitBag\SyliusElasticsearchPlugin\Integration\Repository;
+
+
+use ApiTestCase\JsonApiTestCase;
+use BitBag\SyliusElasticsearchPlugin\Repository\ProductAttributeValueRepository;
+use BitBag\SyliusElasticsearchPlugin\Repository\TaxonRepository;
+use Sylius\Bundle\CoreBundle\Doctrine\ORM\AttributeRepository;
+use Tests\BitBag\SyliusElasticsearchPlugin\Integration\IntegrationTestCase;
+
+class ProductAttributeValueRepositoryTest extends IntegrationTestCase
+{
+    /** @var ProductAttributeValueRepository */
+    private $productAttributeValueRepository;
+
+
+    /** @var AttributeRepository */
+    private $attributeRepository;
+
+    /** @var TaxonRepository */
+    private $taxonRepository;
+
+    public function SetUp(): void
+    {
+        parent::SetUp();
+
+        $this->productAttributeValueRepository = self::$container->get('bitbag.sylius_elasticsearch_plugin.repository.product_attribute_value_repository');
+        $this->attributeRepository = self::$container->get('sylius.repository.product_attribute');
+        $this->taxonRepository = self::$container->get('sylius.repository.taxon');
+    }
+    public function test_get_unique_attribute_values(): void
+    {
+        $this->loadFixturesFromFiles(['Repository/ProductAttributeValueRepositoryTest/test_product_attribute_value_repository.yaml']);
+
+        $attribute = $this->attributeRepository->findAll()[0];
+        $taxon = $this->taxonRepository->findAll()[0];
+
+        $result = $this->productAttributeValueRepository->getUniqueAttributeValues($attribute, $taxon);
+
+        $this->assertNotEmpty($result);
+        $this->assertEquals(1, \count($result));
+    }
+}
