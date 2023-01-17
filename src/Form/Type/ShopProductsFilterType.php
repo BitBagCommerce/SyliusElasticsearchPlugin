d6b7505999036586ea68d08e9c8bd02fec435151
Wojdylak <karolwojdyla37@gmail.com>
2023-01-17 12:22:03 +0100

    OP-57 fix of sorting and products limit

--- src/Form/Type/ShopProductsFilterType.php
+++ src/Form/Type/ShopProductsFilterType.php
@@ -19,7 +19,6 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 final class ShopProductsFilterType extends AbstractFilterType
 {
@@ -45,13 +44,6 @@ final class ShopProductsFilterType extends AbstractFilterType
             ->setMethod('GET');
 
         $builder->addEventListener(FormEvents::PRE_SUBMIT, [$this, 'addFacets']);
-    }
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'csrf_protection' => false,
-        ]);
     }
 
     public function addFacets(FormEvent $event): void
