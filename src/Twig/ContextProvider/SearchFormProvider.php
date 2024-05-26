2fa4ad2d65b8e49de2897b1e0b19d0508efdb328
Gracjan Józefczyk <juzkos@gmail.com>
2024-05-26 18:48:20 +0200

    OP-225: Replace Sonata with Sylius event

---
+++ src/Twig/ContextProvider/SearchFormProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusElasticsearchPlugin\Twig\ContextProvider;
+
+use BitBag\SyliusElasticsearchPlugin\Form\Type\SearchType;
+use Sylius\Bundle\UiBundle\ContextProvider\ContextProviderInterface;
+use Sylius\Bundle\UiBundle\Registry\TemplateBlock;
+use Symfony\Component\Form\FormFactoryInterface;
+
+final class SearchFormProvider implements ContextProviderInterface
+{
+    private const EVENT_NAME = 'sylius.shop.layout.header.content';
+
+    private const BLOCK_NAME = 'bitbag_es_search_form';
+
+    public function __construct(
+        private FormFactoryInterface $formFactory,
+    ) {
+    }
+
+    public function provide(array $templateContext, TemplateBlock $templateBlock): array
+    {
+        $form = $this->formFactory->create(SearchType::class);
+        $templateContext['form'] = $form->createView();
+
+        return $templateContext;
+    }
+
+    public function supports(TemplateBlock $templateBlock): bool
+    {
+        return self::EVENT_NAME === $templateBlock->getEventName()
+            && self::BLOCK_NAME === $templateBlock->getName();
+    }
+}
