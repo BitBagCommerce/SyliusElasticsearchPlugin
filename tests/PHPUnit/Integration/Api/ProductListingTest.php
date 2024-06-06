396b423251ae11278b6925699edb28511eec7392
Gracjan Józefczyk <juzkos@gmail.com>
2024-06-06 15:01:15 +0200

    OP-303: Fix test paths

--- tests/PHPUnit/Integration/Api/ProductListingTest.php
+++ tests/PHPUnit/Integration/Api/ProductListingTest.php
@@ -4,15 +4,26 @@ declare(strict_types=1);
 
 namespace PHPUnit\Integration\Api;
 
+use ApiTestCase\JsonApiTestCase;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Process\Process;
-use Tests\BitBag\SyliusElasticsearchPlugin\Integration\IntegrationTestCase;
 
-final class ProductListingTest extends IntegrationTestCase
+final class ProductListingTest extends JsonApiTestCase
 {
+    public function __construct(
+        ?string $name = null,
+        array $data = [],
+        string $dataName = ''
+    ) {
+        parent::__construct($name, $data, $dataName);
+
+        $this->dataFixturesPath = __DIR__ . \DIRECTORY_SEPARATOR . 'DataFixtures' . \DIRECTORY_SEPARATOR . 'ORM';
+        $this->expectedResponsesPath = __DIR__ . \DIRECTORY_SEPARATOR . 'Responses' . \DIRECTORY_SEPARATOR . 'Expected';
+    }
+
     public function test_it_finds_products_by_name(): void
     {
-        $this->loadFixturesFromFiles(['Api/test_it_finds_products_by_name.yaml']);
+        $this->loadFixturesFromFiles(['test_it_finds_products_by_name.yaml']);
         $this->populateElasticsearch();
 
         $this->client->request('GET', '/api/v2/shop/products/search?query=mug');
@@ -22,7 +33,7 @@ final class ProductListingTest extends IntegrationTestCase
 
     public function test_it_finds_products_by_name_and_facets(): void
     {
-        $this->loadFixturesFromFiles(['Api/test_it_finds_products_by_name_and_facets.yaml']);
+        $this->loadFixturesFromFiles(['test_it_finds_products_by_name_and_facets.yaml']);
         $this->populateElasticsearch();
 
         $this->client->request('GET', '/api/v2/shop/products/search?query=mug&facets[color][]=red');
